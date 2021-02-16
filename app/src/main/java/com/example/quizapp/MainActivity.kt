c9e4c54cae8a4152eed287b10aae2f1dea35c575
gaku yamamoto <githubgemini1124@gmail.com>
2021-02-17 02:39:42 +0900

    implement intent

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.btn_start)
+        val etName = findViewById<TextView>(R.id.et_name)
+        button.setOnClickListener {
+            if (etName.text.toString().isEmpty()) {
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_LONG).show()
+            }else {
+                val intent = Intent(this, QuizQuestionActivity::class.java)
+                startActivities(arrayOf(intent))
+                finish()
+            }
+        }
     }
 }
